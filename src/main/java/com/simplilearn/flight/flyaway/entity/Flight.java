327111faf50b64fab8d35b3cdab969829e741b0e
Deshatwar, Sachin <sachin_deshatwar@dell.com>
2020-04-20 13:11:04 +0530

    first push

---
+++ src/main/java/com/simplilearn/flight/flyaway/entity/Flight.java
@@ -0,0 +1,90 @@
+package com.simplilearn.flight.flyaway.entity;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+@Entity
+public class Flight {
+
+	@Id
+	private String id;
+	private String departure;
+	private String arrival;
+	private String  departureDate;
+	private String arrivalDate;
+	
+	@ManyToMany(mappedBy = "flights", fetch = FetchType.LAZY)
+	private Set<FlightBooking> bookings;
+
+	public Flight() {
+		super();
+	}
+
+	public Flight(String id, String departure, String arrival, String departureDate, String arrivalDate) {
+		super();
+		this.id = id;
+		this.departure = departure;
+		this.arrival = arrival;
+		this.departureDate = departureDate;
+		this.arrivalDate = arrivalDate;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDeparture() {
+		return departure;
+	}
+
+	public void setDeparture(String departure) {
+		this.departure = departure;
+	}
+
+	public String getArrival() {
+		return arrival;
+	}
+
+	public void setArrival(String arrival) {
+		this.arrival = arrival;
+	}
+
+	public String getDepartureDate() {
+		return departureDate;
+	}
+
+	public void setDepartureDate(String departureDate) {
+		this.departureDate = departureDate;
+	}
+
+	public String getArrivalDate() {
+		return arrivalDate;
+	}
+
+	public void setArrivalDate(String arrivalDate) {
+		this.arrivalDate = arrivalDate;
+	}
+
+	public Set<FlightBooking> getBookings() {
+		return bookings;
+	}
+
+	public void setBookings(Set<FlightBooking> bookings) {
+		this.bookings = bookings;
+	}
+
+	@Override
+	public String toString() {
+		return "Flight [id=" + id + ", departure=" + departure + ", arrival=" + arrival + ", departureDate="
+				+ departureDate + ", arrivalDate=" + arrivalDate + "]";
+	}
+
+}
